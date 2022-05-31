71cd7ce74352313eaa425beef068de640ca01596
BarraR3port <barrar3port@gmail.com>
2022-05-31 02:33:34 -0400

    Delete common/target directory

--- common/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.lymarket.common"},{"l":"net.lymarket.common.commands"},{"l":"net.lymarket.common.config"},{"l":"net.lymarket.common.db"},{"l":"net.lymarket.common.error"},{"l":"net.lymarket.common.lang"},{"l":"net.lymarket.common.version"}];updateSearchResults();
