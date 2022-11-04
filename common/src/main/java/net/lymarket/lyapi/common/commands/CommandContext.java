3dadb25542c90ad1bd952057b5c6ba9917346312
BarraR3port <barrar3port@gmail.com>
2022-11-03 23:37:45 -0300

    Changelog: + Updated the Copyright. - Removed the discord workflow.

--- common/src/main/java/net/lymarket/lyapi/common/commands/CommandContext.java
+++ common/src/main/java/net/lymarket/lyapi/common/commands/CommandContext.java
@@ -1,14 +1,12 @@
 /*
- * BSD 3-Clause License
- *
- * Copyright (c) 2022, LyMarket
+ * Copyright (c) 2022. BSD 3-Clause License, BarraR3port.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * https://github.com/Lydark-Studio/LyApi/blob/master/LICENSE
+ * https://github.com/BarraR3port/LyApi/blob/master/LICENSE
  *
- * Contact: contact@lymarket.net
+ * Contact: barrar3port@gmail.com
  */
 
 package net.lymarket.lyapi.common.commands;
--- lyapi-spigot/src/main/java/net/lymarket/lyapi/spigot/config/ReloadableConfig.java
+++ lyapi-spigot/src/main/java/net/lymarket/lyapi/spigot/config/ReloadableConfig.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2022. BSD 3-Clause License, BarraR3port.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * https://github.com/BarraR3port/LyApi/blob/master/LICENSE
+ *
+ * Contact: barrar3port@gmail.com
+ */
+
 package net.lymarket.lyapi.spigot.config;
 
 
